1dbdc54983c540e89440d56ce4b3c53df78bd75f
mmendespro <marcelo.mendes.gomes@gmail.com>
2023-01-27 07:33:49 -0300

    improve build script

--- build-project.sh
+++ build-project.sh
@@ -11,4 +11,4 @@ docker rmi spring/spring-cqrs-poc-jvm:latest
 # build container image
 docker build -f docker/Dockerfile.jvm -t spring/spring-cqrs-poc-jvm .
 # run container
-docker run -id --network=metrics --name spring-cqrs-poc-jvm -p 8080:8080 spring/spring-cqrs-poc-jvm
+docker run -id --network=poc-net --name spring-cqrs-poc-jvm -p 8080:8080 spring/spring-cqrs-poc-jvm
